83ecf53c444548df5ebd074729d64675c8da548a
Dinesh Pandey <dineshpandey3536@gmail.com>
2022-11-06 20:22:07 -0600

    complected the user Auth

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
+import { signOut } from '@firebase/auth'
 import React from 'react'
+import { auth } from "../firebase"
 
 const Navbar = () => {
   return (
@@ -7,7 +9,7 @@ const Navbar = () => {
         <div className="user">
           <img src="https://wallpaper.dog/large/17068480.jpg" alt="" />
           <span>Dinesh</span>
-          <button>logout</button>
+          <button onClick={()=>signOut(auth)}>logout</button>
         </div>
     </div>
   )
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getStorage} from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA8MbpixiyqoOvpCdZLSs-_Is_IThExLHc",
@@ -14,3 +15,7 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+
+// Create a root reference
+export const storage = getStorage();
+export const db = getFirestore();
