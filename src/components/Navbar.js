5356a5e210384c3aeb775c7837b22dc34ec3954d
Dinesh Pandey <dineshpandey3536@gmail.com>
2022-11-08 21:53:31 -0600

    added the login and signup functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
 import { signOut } from '@firebase/auth'
-import React from 'react'
+import React, { useContext } from 'react'
 import { auth } from "../firebase"
+import { AuthContext } from '../context/AuthContext'
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext);
+
   return (
     <div className='navbar'>
         <span className="logo">Chat Application</span>
         <div className="user">
-          <img src="https://wallpaper.dog/large/17068480.jpg" alt="" />
-          <span>Dinesh</span>
+          <img src={currentUser.photoURL} alt="" />
+          <span>{currentUser.displayName}</span>
           <button onClick={()=>signOut(auth)}>logout</button>
         </div>
     </div>
